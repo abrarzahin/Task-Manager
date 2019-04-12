f8ce54627b8d8ed32084e9276ff05c44f7bc9b49
abrar zahin <abrarzahin@yahoo.com>
2019-04-11 23:10:52 -0400

    4 router added

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap'
 import '@/assets/css/style.css'
 
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/tasks/TasksAll.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Task All</h1>
+</template>
