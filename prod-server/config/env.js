170f571a6cc06b223ab5e9b22fec5a9f4c1d04e8
abrar zahin <abrarzahin@yahoo.com>
2019-04-19 22:56:13 -0400

    10 middleware and cors

---
+++ prod-server/config/env.js
@@ -0,0 +1,57 @@
+"use strict";
+
+var _interopRequireDefault = require("C:\\Users\\Abrar Zahin\\Desktop\\taskman\\node_modules\\@babel\\runtime-corejs2/helpers/interopRequireDefault");
+
+var _Object$defineProperty = require("C:\\Users\\Abrar Zahin\\Desktop\\taskman\\node_modules\\@babel\\runtime-corejs2/core-js/object/define-property");
+
+_Object$defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports.setEnvironment = setEnvironment;
+
+require("core-js/modules/es6.regexp.to-string");
+
+var _express = _interopRequireDefault(require("express"));
+
+var _morgan = _interopRequireDefault(require("morgan"));
+
+var _cors = _interopRequireDefault(require("cors"));
+
+var _bodyParser = _interopRequireDefault(require("body-parser"));
+
+/**
+ * Determines the current environment and sets the appropriate variables
+ * @param {Express App} app 
+ */
+function setEnvironment(app) {
+  if (!process.env.NODE_ENV || process.env.NODE_ENV.toString().trim() !== 'production') {
+    setDevEnv(app);
+  } else {
+    setProdEnv(app);
+  }
+}
+/**
+ * Used to set development environment variables
+ * @param {Express App} app 
+ */
+
+
+function setDevEnv(app) {
+  process.env.NODE_ENV = 'development';
+  app.use(_bodyParser.default.json()); // Allows parsing JSON from the client
+
+  app.use((0, _morgan.default)('dev')); // Log HTTP Requests to the node console (for debugging purposes)
+
+  app.use((0, _cors.default)()); // Enable Cross Origin Requests, since Vue.JS is on a different origin
+}
+/**
+ * Used to set production environment variables
+ * @param {Express App} app 
+ */
+
+
+function setProdEnv(app) {
+  app.use(_bodyParser.default.json());
+  app.use(_express.default.static(__dirname + '/../../dist'));
+}
--- prod-server/index.js
+++ prod-server/index.js
@@ -2,15 +2,28 @@
 
 var _interopRequireDefault = require("C:\\Users\\Abrar Zahin\\Desktop\\taskman\\node_modules\\@babel\\runtime-corejs2/helpers/interopRequireDefault");
 
+require("core-js/modules/es6.regexp.to-string");
+
 var _express = _interopRequireDefault(require("express"));
 
 var _routes = require("./routes");
 
+var _env = require("./config/env");
+
 var app = (0, _express.default)();
+(0, _env.setEnvironment)(app);
 (0, _routes.registerRoutes)(app);
 app.get('/', function (req, res) {
-  return res.send('Hello World!');
-});
+  if (!process.env.NODE_ENV || process.env.NODE_ENV.toString().trim() !== 'production') {
+    return res.send('Running server in development mode.');
+  } else {
+    // Returns the main index file in production environment
+    return res.sendFile('index.html', {
+      root: __dirname + '/../dist/'
+    });
+  }
+}); // Starts the server on the given port
+
 app.listen(3000, function () {
-  return console.log("This app listening on port 3000!");
+  console.log('MEVN app listening on port 3000 in ' + process.env.NODE_ENV + ' mode!');
 });
